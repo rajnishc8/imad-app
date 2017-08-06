0c68aaa3f78f42e4343624e47cd3e75d4daffb51
rajnishc8 <rajnishc8@gmail.com>
2017-08-06 17:47:54 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -39,10 +39,54 @@ button.onclick = function ()
     request.send(null);
 }
 
+//Submit name
+
+var submit = document.getElementById('submit_btn');
+submit.onclick = function ()
+{
+    //Make a request to the server and send the name
+    var request = new XMLHttpRequest();
+    
+    // capture the response and store it in a variable
+    request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  //render the variable in the correct span
+                  var names = request.responseText;
+                  names = JSON.parse(names)
+                  // Capture a list of names and render it as a list
+                  var list = '';
+                  for (var i=0; i< names.length; i++)
+                  {
+                      list += '<li>' + names[i] + '</li>'; 
+                  }
+                  //submit.value = 'Sucess!';
+              } else if (request.status === 403) {
+                  //submit.value = 'Invalid credentials. Try again?';
+              } else if (request.status === 500) {
+                  alert('Something went wrong on the server');
+                  //submit.value = 'Login';
+              } else {
+                  alert('Something went wrong on the server');
+                  //submit.value = 'Login';
+              }
+              //loadLogin();
+          }  
+          // Not done yet
+        };
+    
+    //Make the request
+    var  nameInput = document.getElementById('name');
+    var  name = nameInput.value;
+    request.open('GET', 'http://rajnishc8.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(null);
+}
+
 element.innerHTML = 'New Value';
 
 //Move the image
-var img = document.getElementById('madi');
+var img = document.getElementById('ProfilePic');
 var marginLeft = 0;
 function moveRight()
 { 
@@ -54,3 +98,4 @@ img.onclick = function ()
 {
    var interval = setInterval(moveRight, 50);
 }
+
