cb9975ea5d28116291adf30d460aa35284a28a8e
rajnishc8 <rajnishc8@gmail.com>
2017-08-27 00:07:54 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -242,8 +242,8 @@ app.get('/test-db', function (req, res) {
 
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
-async function lsExample() {
-  const { stdout, stderr } = await exec('ls');
+function lsExample() {
+  const { stdout, stderr } = exec('ls');
   console.log('stdout:', stdout);
   console.log('stderr:', stderr);
 }
